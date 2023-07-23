2a52c39ee3b2cf7fe6611e2c39487dc2a7137616
Hasan Erken <hasan.erken@gmail.com>
2023-07-23 22:51:18 +0300

    initial

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+func main() {
+	// Create new Fiber instance
+	app := fiber.New()
+
+	// Create new GET route on path "/hello"
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	// Start server on http://localhost:3000
+	log.Fatal(app.Listen(":3000"))
+
+	//   DATABASE_URL=postgres://xfiber:nay4LTqtq4epj9Q@xfiber-db.flycast:5432/xfiber?sslmode=disable
+}
